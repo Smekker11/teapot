752fa8b649c1c031876a03c99c7d4d39958174a2
Mekker David <mekkerdavid@gmail.com>
2025-12-10 12:07:14 +0200

    redesign website

--- error.php
+++ error.php
@@ -1,13 +1,27 @@
 <?php
 header("HTTP/1.1 418 I'm a teapot");
 ?>
-<html>
-<h1 align="center">418 I'm a teapot</h1>
-<hr>
-<p align="center"> HYPER TEXT COFFEE POT CONTROL PROTOCOL RUNNING ON:</p>
-<p align="center">
-<?php
- echo $_SERVER['SERVER_SOFTWARE']
-?>
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>418 I'm a teapot</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="menu-container">
+        <div class="error-container">
+            <div class="error-code">418</div>
+            <h1 class="error-title">I'm a teapot</h1>
+            <p class="error-message">The HTCPCP server is brewing coffee,<br> not serving HTTP requests!</p>
+            
+            <hr style="border: none; height: 1px; background: rgba(255, 255, 255, 0.2); margin: 24px 0;">
+            
+            <strong>Hyper Text Coffee Pot Control Protocol Server: <br></strong>
+            <p class="error-message"><?php echo htmlspecialchars($_SERVER['SERVER_SOFTWARE']); ?></p>
+            <a href="index.php" class="back-button">Back to Server</a>
+        </div>
+    </div>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,21 +1,36 @@
 <?php
 function tea(){
-    $location = '/teapot/error.php';
-    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . "/" . $location);
+    $location = '/error.php';
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 }
 if (isset($_GET['tea'])) {
     tea();
   }
 ?>
-<html>
- <body>
-     <h1 style="color: yellow;">HTCPCP SERVER:</h1>
-     <img height="200" width="200" src="coffee-pot.png"></img>
-     <button onclick="window.location.href='index.php?tea=true'">BREW</button>
- </body>
- <style>
-     body{
-         background-image: url('coffee-bean.bmp');
-     }
- </style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTCPCP Server</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="menu-container">
+        <div class="menu-header">
+            <img class="menu-image" src="coffee-pot.png" alt="Coffee Pot">
+            <h1 class="menu-title">HTCPCP Server</h1>
+            <p class="menu-subtitle">Hyper Text Coffee Pot Control Protocol</p>
+        </div>
+        <hr style="border: none; height: 1px; background: rgba(255, 255, 255, 0.2); margin: 24px 0;">
+        <nav class="menu-items">
+            <button class="menu-item primary" onclick="window.location.href='index.php?tea=true'">
+                BREW
+            </button>
+            <a class="menu-item" href="https://tools.ietf.org/html/rfc2324" target="_blank">
+                RFC 2324
+            </a>
+        </nav>
+    </div>
+</body>
 </html>
